b4628e2ef9fa41ac3638a7451e5bbe3aa17f7e8f
vde-axelor <v.deasi@axelor.com>
2023-01-23 15:32:47 +0530

    Simple Guice Example

--- src/main/java/com/impl/SpellCheckerImpl.java
+++ src/main/java/com/impl/SpellCheckerImpl.java
@@ -1,20 +1,10 @@
 package com.impl;
 
 import com.cls.SpellChecker;
-import com.google.inject.name.Named;
 
 public class SpellCheckerImpl implements SpellChecker{
-	private String name;
-
-	   public SpellCheckerImpl(){}
-
-	   public SpellCheckerImpl(@Named("Name") String name){
-	      this.name = name;
+	 @Override
+	   public void checkSpelling() {
+	      System.out.println("Hello" );
 	   } 
-
-	   @Override
-	   public void checkSpelling() { 
-	      System.out.println("Inside checkSpelling." );
-	      System.out.println(name); 
-	   }
 }
--- src/main/java/com/module/TextEditorModule.java
+++ src/main/java/com/module/TextEditorModule.java
@@ -2,19 +2,11 @@ package com.module;
 
 import com.cls.SpellChecker;
 import com.google.inject.AbstractModule;
-import com.google.inject.name.Names;
 import com.impl.SpellCheckerImpl;
 
 public class TextEditorModule extends AbstractModule{
 	@Override
 	   protected void configure() {
-	      try {
-	         bind(SpellChecker.class).toConstructor(SpellCheckerImpl.class.getConstructor(String.class));
-	      } catch (NoSuchMethodException | SecurityException e) {
-	         System.out.println("Required constructor missing");
-	      } 
-	      bind(String.class)
-	         .annotatedWith(Names.named("Name"))
-	         .toInstance("Hello...");
+	      bind(SpellChecker.class).to(SpellCheckerImpl.class);
 	   } 
 }
