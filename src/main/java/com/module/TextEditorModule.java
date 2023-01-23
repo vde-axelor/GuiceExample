c92f358cfffb3ed443d76a35c96bf958cfdd578b
vde-axelor <v.deasi@axelor.com>
2023-01-23 15:37:10 +0530

    constructor binding

--- src/main/java/com/module/TextEditorModule.java
+++ src/main/java/com/module/TextEditorModule.java
@@ -2,11 +2,19 @@ package com.module;
 
 import com.cls.SpellChecker;
 import com.google.inject.AbstractModule;
+import com.google.inject.name.Names;
 import com.impl.SpellCheckerImpl;
 
 public class TextEditorModule extends AbstractModule{
 	@Override
 	   protected void configure() {
-	      bind(SpellChecker.class).to(SpellCheckerImpl.class);
-	   } 
+	      try {
+	         bind(SpellChecker.class).toConstructor(SpellCheckerImpl.class.getConstructor(String.class));
+	      } catch (NoSuchMethodException | SecurityException e) {
+	         System.out.println("Required constructor missing");
+	      } 
+	      bind(String.class)
+	         .annotatedWith(Names.named("Name"))
+	         .toInstance("Hello...");
+	   }  
 }
